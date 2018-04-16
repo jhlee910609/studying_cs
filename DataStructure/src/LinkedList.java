f4a86121934a3b7f6fd36aa372e44e514695b4bf
Lee Jun Hee <>
2018-04-16 18:07:37 +0900

    add linked list code

---
+++ DataStructure/src/LinkedList.java
@@ -0,0 +1,87 @@
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void insertMiddleNode(Node pre, String data) {
+        Node newNode = new Node(data);
+        newNode.link = pre.link;
+        pre.link = newNode;
+    }
+
+    public void insertLastNode(String data) {
+        if (head == null) {
+            head = new Node(data);
+        } else {
+            Node newNode = new Node(data);
+            Node tempNode = head;
+            while (tempNode.link != null) {
+                tempNode = tempNode.link;
+            }
+            tempNode.link = newNode;
+        }
+    }
+
+    public void deleteLastNode() {
+        if (head == null) {
+            System.out.println("더 이상 삭제할 노드가 없습니다.");
+            return;
+        }
+
+        if (head.link == null)
+            head = null;
+
+        else {
+            Node pre, temp;
+
+            pre = head;
+            temp = head.link;
+
+            while (temp.link != null) {
+                pre = temp;
+                temp = pre.link;
+            }
+            pre.link = null;
+        }
+    }
+
+    public Node searchNode(String data) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.equals(data))
+                return temp;
+            else
+                temp = temp.link;
+        }
+        return temp;
+    }
+
+    public void reverseList() {
+        Node next = head;
+        Node current = null;
+        Node pre = null;
+
+        while (next != null) {
+            pre = current;
+            current = next;
+            next = next.link;
+            current.link = pre;
+        }
+        head = current;
+    }
+
+    public void printList() {
+        Node temp = head;
+        System.out.println("========== [ node list ] ==========");
+        while (temp.link != null) {
+            System.out.println("========== [ " + temp.data + " ] ==========");
+            temp = temp.link;
+        }
+        System.out.println("============================");
+    }
+}
+
+
---
+++ DataStructure/src/Node.java
@@ -0,0 +1,22 @@
+import java.security.PublicKey;
+
+public class Node {
+
+    public String data;
+    public Node link;
+
+    public Node() {
+        this.data = null;
+        this.link = null;
+    }
+
+    public Node(String data, Node link) {
+        this.data = data;
+        this.link = link;
+    }
+
+    public Node(String data) {
+        this.data = data;
+        this.link = null;
+    }
+}
