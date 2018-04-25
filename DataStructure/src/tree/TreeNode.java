d38fbdd4d0099780e7bf5e4a1476db14cc0cd527
Lee Jun Hee <>
2018-04-25 16:50:55 +0900

    add TreeNode

---
+++ DataStructure/src/tree/TreeNode.java
@@ -0,0 +1,46 @@
+package tree;
+
+
+// 이진순회 자료구조
+// TreeNode 기본구조
+public class TreeNode {
+    Object data;
+    TreeNode left;
+    TreeNode right;
+}
+
+class LinkedTree {
+    private TreeNode root;
+
+    public TreeNode makeBT(TreeNode bt1, TreeNode bt2, Object data) {
+        TreeNode root = new TreeNode();
+        this.root.data = data;
+        this.root.left = bt1;
+        this.root.right = bt2;
+        return root;
+    }
+
+    public void preorder(TreeNode root) {
+        if (root != null) {
+            System.out.printf("%c", root.data);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    public void inoder(TreeNode root) {
+        if (root != null) {
+            inoder(root.left);
+            System.out.printf("%c", root.data);
+            inoder(root.right);
+        }
+    }
+
+    public void postorder(TreeNode root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.printf("%c", root.data);
+        }
+    }
+}
